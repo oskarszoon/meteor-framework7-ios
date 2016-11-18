98b9a1ea68616176d05bc110cb8c151e0d093553
Oli Oskarsson <oli@oskarsson.nl>
2016-11-18 17:40:44 +0100

    Upped to v1.5.0

--- package.js
+++ package.js
@@ -1,7 +1,7 @@
 Package.describe({
     name: 'loftsteinn:framework7-ios',
-    version: '1.4.2',
-    summary: 'Framework7 v1.4.2 for Meteor using the iOS theme',
+    version: '1.5.0',
+    summary: 'Framework7 v1.5.0 for Meteor using the iOS design theme',
     git: 'https://github.com/oskarszoon/meteor-framework7-ios',
     documentation: 'README.md'
 });
@@ -15,16 +15,6 @@ Package.onUse(function (api) {
     ], 'client');
 
     api.addAssets([
-        'framework7/dist/img/i-f7-ios.png',
-        'framework7/dist/img/i-form-calendar-ios.svg',
-        'framework7/dist/img/i-form-comment-ios.svg',
-        'framework7/dist/img/i-form-email-ios.svg',
-        'framework7/dist/img/i-form-gender-ios.svg',
-        'framework7/dist/img/i-form-name-ios.svg',
-        'framework7/dist/img/i-form-password-ios.svg',
-        'framework7/dist/img/i-form-settings-ios.svg',
-        'framework7/dist/img/i-form-tel-ios.svg',
-        'framework7/dist/img/i-form-toggle-ios.svg',
-        'framework7/dist/img/i-form-url-ios.svg'
+        'framework7/dist/img/i-f7-ios.png'
     ], 'client');
 });
